26485dc8772da0e012004c6e9f994bd29e2394aa
Ravi <ravipradhan@protonmail.com>
2021-05-04 18:48:23 +0100

    start setting up scss

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'prettier'
+gem 'sassc'
 gem 'sinatra'
 gem 'rake'
 gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
-require "sinatra"
+require 'sinatra'
 
 class App < Sinatra::Base
-    get "/" do
-        erb :index
-    end
+  get '/' do
+    erb :index
+  end
 end
